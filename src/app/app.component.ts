3ef86d471cd09349181ac5740193a603169cbd77
vaibhavmehtaa <vaibhav.mehta@vogo.in>
2019-02-09 13:23:18 +0530

    Fixed:(login user access)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LayoutService } from './components/layout/_services/layout.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'x-admin';
+  user = undefined;
+
+  constructor(
+    private _layoutServices: LayoutService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this._layoutServices.currentUser$.subscribe((userData) => {
+      this.user = (userData) ? userData : this.router.navigate(['/login']);
+    });
+  }
 }
--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { LayoutService } from '../_services/layout.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'x-header',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  @Input() userInfo = this._layoutService.currentUsers;
+  constructor(
+    private _layoutService: LayoutService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+
   }
 
+  logout($e) {
+    this._layoutService.purgeAuth();
+    this.router.navigate(['/login']);
+  }
+
+  isAuthorized(user) {
+    if (typeof user === "string") {
+      let info = JSON.parse(user);
+      return (info.token);
+    }else if (user.token) {
+      return true;
+    }
+    return false;
+  }
 }
